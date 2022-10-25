c29f8ef583fc9c7136e076d09533cbd8bb362052
allen-lhj <liu772636821@163.com>
2022-10-25 10:20:22 +0800

    age-calculator

--- Digital-Clock/index.js
+++ Digital-Clock/index.js
@@ -1,11 +1,11 @@
-const degree = 6;
+const degree = 360 / 60;
 const hr = document.querySelector('#hr');
 const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
 
 setInterval(() => {
   let day = new Date();
-  let hh = day.getHours() * 30;
+  let hh = day.getHours() * (360 / 12);
   let mm = day.getMinutes() * degree;
   let ss = day.getSeconds() * degree;
 
